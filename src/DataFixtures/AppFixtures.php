f398fa72cddf0189ed45b0620bbaa63911252708
Alexandre Gaillot <contact@steptosuccess.fr>
2022-09-27 16:25:10 +0200

    correction exercice => creation entité Post et Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,31 +3,52 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use Faker\Generator;
 use App\Entity\Category;
+use App\Entity\Post;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        // On charge le générateur de Faker
+        $this->faker = Factory::create();
+    }
+
     public function load(ObjectManager $manager): void
     {
         $this->createCategory($manager);
+        $this->createPost($manager);
         // On exécute toutes els requêtes en attente puis on vide la file.
         $manager->flush();
     }
 
     private function createCategory (ObjectManager $manager): void
     {
-        // On charge le générateur de Faker
-        $faker = Factory::create();
         // On lance une boucle pour créer 10 catégories
         for ($i=0; $i < 10; $i++) { 
             // On instancie l'entité Category pour générer nos catégories
             $category = new Category();
             // On donne un nom à la catégorie généré par Faker
-            $category->setName($faker->words(3, true));
+            $category->setName($this->faker->words(3, true));
             // On ajoute la catégorie en file d'attente pour être enregistrer en BDD
             $manager->persist($category);
+        }
+    }
+
+    private function createPost (ObjectManager $manager): void
+    {
+        for ($i=0; $i < 100; $i++) { 
+            $post = new Post;
+            $post->setTitle($this->faker->words(5, true))
+                ->setDescription($this->faker->paragraphs(5, true))
+                ->setCreatedAt($this->faker->dateTime());
+            
+            $manager->persist($post);
         }
     }
 }
