12105782d7e5f70c4adb434446fe5d1e583aa0e0
Alexandre Gaillot <contact@steptosuccess.fr>
2022-09-28 16:56:08 +0200

    formulaire et fichier FormType

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Ce controller a été généré grâce à la commande 'symfony console make:controller'
@@ -33,6 +35,28 @@ class PostController extends AbstractController
     {
         return $this->render('post/single.html.twig', [
             'post' => $this->manager->getRepository(Post::class)->find($id)
+        ]);
+    }
+
+    #[Route('/post/save', name:"add_post", methods:['GET', 'POST'])]
+    public function save(Request $request): Response
+    {
+        $post = new Post;
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $post->setCreatedAt(new \DateTime);
+            $om = $this->manager->getManager();
+            $om->persist($post);
+            $om->flush();
+
+            return $this->redirectToRoute("single_post", ['id' => $post->getId()]);
+        }
+
+        return $this->renderForm("post/save.html.twig", [
+            'PostForm' => $form
         ]);
     }
 }
